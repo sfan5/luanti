fd3471a8b07e405ae61ad66952cc03d2c9076a41
sfan5 <sfan5@live.de>
2026-07-29 09:50:19 +0200

    keep counters
    saves another 1-2ms

--- src/client/imagefilters.cpp
+++ src/client/imagefilters.cpp
@@ -367,10 +367,11 @@ static void imageTransformInlined(u8 sxn, u8 syn, const video::IImage *src, vide
 	const u32 src_stride = src->getDimension().Width;
 	const core::dimension2du dst_dim = dst->getDimension();
 
-	for (u32 dy = 0; dy < dst_dim.Height; dy++)
-	for (u32 dx = 0; dx < dst_dim.Width; dx++) {
-		u32 entries[4] = {dx, dst_dim.Width-1U-dx, dy, dst_dim.Height-1U-dy};
-		u32 sx = entries[sxn], sy = entries[syn];
+	u32 pos[4]; // [x+, x-, y+, y-]
+	for (pos[0] = 0, pos[1] = dst_dim.Height-1; pos[0] < dst_dim.Height; pos[0]++, pos[1]--)
+	for (pos[2] = 0, pos[3] = dst_dim.Width-1; pos[2] < dst_dim.Width; pos[2]++, pos[3]--)
+	{
+		u32 sx = pos[sxn], sy = pos[syn];
 		*dst_data = src_data[src_stride*sy + sx];
 		dst_data++;
 	}
