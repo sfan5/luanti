sfan5/luanti

commit 5c506adbed9c428f2ba5088884af83c37ab4e1f8

Implement basic gamepad support (#17219)
* Adjust InputHandler to also handle analog input

The touchscreen joystick is also modified to send movement as x/y
coordinates instead of reporting itself as a virtual joystick.

* Remove separate code path for joystick controller

* Introduce new gamepad handler

* Handle joystick deadzone

* Properly handle camera rotation with gamepads

* Rearrange input-related settings

* Repeat gamepad buttons in MyEventReceiver::WasKeyDown

* fixup! Properly handle camera rotation with gamepads

* Deduplicate wasKeyDown for gamepad axes

* Add KeyPress-based abstraction for button/axis input events

* guiButtonKey: allow binding to gamepad buttons and axes

* Correctly handle short joystick presses in wasKeyDown

* Implement KeyPress::name for gamepad buttons and axes

* Drop enable_joysticks setting

* Use SDL_GetGamepadButtonLabel to get more controller-specific labels for A/B/X/Y buttons

* Add keybinding for opening the pause menu in-game

* Allow closing in-game formspecs with keymap_pause

* Update settingtypes

* fixup! Implement KeyPress::name for gamepad buttons and axes

* Introduce InputSourceType::INVALID for invalid KeyPress values

* cleanup diff

* Improve UX related to joystick deadzone

* fixup! Improve UX related to joystick deadzone

* Apply suggestions from code review

Co-authored-by: Lars Müller <34514239+appgurueu@users.noreply.github.com>

* minor: use f64 timestamp

* fixup! Apply suggestions from code review

* minor doxygen

* fixup! Improve UX related to joystick deadzone

* minor annotations

* KeyPress::flip -> KeyPress::getOppositeAxisDirection

* MyEventReceiver::KeyState -> MyEventReceiver::PhysicalKeyState

* settingtypes: header "Gamepad" -> "Gamepad and Joysticks"

Co-authored-by: SmallJoker <SmallJoker@users.noreply.github.com>

* minor: 32767.0 -> (f32)INT16_MAX

Co-authored-by: SmallJoker <SmallJoker@users.noreply.github.com>

* fixup! minor: 32767.0 -> (f32)INT16_MAX

src/client/game.cpp
@@ -10,7 +10,6 @@
 #include "client/inputhandler.h"
 #include "client/texturepaths.h"
 #include "client/keys.h"
-#include "client/joystick_controller.h"
 #include "client/mapblock_mesh.h"
 #include "client/sound.h"
 #include "clientmap.h"
@@ -381,11 +380,10 @@ Game::Game() :
 
 	const char *settings[] = {
 		"chat_log_level", "doubletap_jump", "toggle_sneak_key", "toggle_aux1_key",
-		"enable_joysticks", "enable_fog", "mouse_sensitivity", "joystick_frustum_sensitivity",
+		"enable_fog", "mouse_sensitivity",
 		"repeat_place_time", "repeat_dig_time", "noclip", "free_move", "fog_start",
 		"cinematic", "cinematic_camera_smoothing", "camera_smoothing", "invert_mouse",
 		"enable_hotbar_mouse_wheel", "invert_hotbar_mouse_wheel", "pause_on_lost_focus",
-		"keyboard_camera_speed",
 	};
 	for (auto s : settings)
 		g_settings->registerChangedCallback(s, &settingChangedCallback, this);
@@ -2013,23 +2011,17 @@ void Game::updateCameraOrientation(CameraOrientation *cam, float dtime)
 			input->setMousePos(center.X, center.Y);
 	}
 
-	if (m_cache_enable_joysticks) {
-		f32 c = m_cache_joystick_frustum_sensitivity * dtime * sens_scale;
-		cam->camera_yaw -= input->joystick.getAxisWithoutDead(JA_FRUSTUM_HORIZONTAL) * c;
-		cam->camera_pitch += input->joystick.getAxisWithoutDead(JA_FRUSTUM_VERTICAL) * c;
-	}
-
 	// Keyboard look
-	const f32 rate = m_cache_keyboard_camera_speed * dtime * sens_scale;
+	const f32 rate = dtime * sens_scale;
 
 	if (input->isKeyDown(KeyType::CAMERA_YAW_LEFT))
-		cam->camera_yaw += rate;
+		cam->camera_yaw += input->getAxisValue(KeyType::CAMERA_YAW_LEFT) * rate;
 	if (input->isKeyDown(KeyType::CAMERA_YAW_RIGHT))
-		cam->camera_yaw -= rate;
+		cam->camera_yaw -= input->getAxisValue(KeyType::CAMERA_YAW_RIGHT) * rate;
 	if (input->isKeyDown(KeyType::CAMERA_PITCH_UP))
-		cam->camera_pitch -= rate;
+		cam->camera_pitch -= input->getAxisValue(KeyType::CAMERA_PITCH_UP) * rate;
 	if (input->isKeyDown(KeyType::CAMERA_PITCH_DOWN))
-		cam->camera_pitch += rate;
+		cam->camera_pitch += input->getAxisValue(KeyType::CAMERA_PITCH_DOWN) * rate;
 
 	cam->camera_pitch = rangelim(cam->camera_pitch, -90, 90);
 }
@@ -2057,10 +2049,10 @@ void Game::updatePlayerControl(const CameraOrientation &cam)
 	//TimeTaker tt("update player control", NULL, PRECISION_NANO);
 
 	PlayerControl control(
-		isKeyDown(KeyType::FORWARD),
-		isKeyDown(KeyType::BACKWARD),
-		isKeyDown(KeyType::LEFT),
-		isKeyDown(KeyType::RIGHT),
+		getAxisValue(KeyType::FORWARD),
+		getAxisValue(KeyType::BACKWARD),
+		getAxisValue(KeyType::LEFT),
+		getAxisValue(KeyType::RIGHT),
 		isKeyDown(KeyType::JUMP) || player->getAutojump(),
 		getTogglableKeyState(KeyType::AUX1,  m_cache_toggle_aux1_key, player->control.aux1),
 		getTogglableKeyState(KeyType::SNEAK, allow_sneak_toggle,      player->control.sneak),
@@ -2068,9 +2060,7 @@ void Game::updatePlayerControl(const CameraOrientation &cam)
 		isKeyDown(KeyType::DIG),
 		isKeyDown(KeyType::PLACE),
 		cam.camera_pitch,
-		cam.camera_yaw,
-		input->getJoystickSpeed(),
-		input->getJoystickDirection()
+		cam.camera_yaw
 	);
 	control.setMovementFromKeys();
 
@@ -2820,12 +2810,6 @@ void Game::processPlayerInteraction(f32 dtime, bool show_hud)
 	// Ensure DIG & PLACE are marked as handled
 	wasKeyDown(KeyType::DIG);
 	wasKeyDown(KeyType::PLACE);
-
-	input->joystick.clearWasKeyPressed(KeyType::DIG);
-	input->joystick.clearWasKeyPressed(KeyType::PLACE);
-
-	input->joystick.clearWasKeyReleased(KeyType::DIG);
-	input->joystick.clearWasKeyReleased(KeyType::PLACE);
 }
 
 
@@ -3748,11 +3732,8 @@ void Game::readSettings()
 	m_cache_doubletap_jump               = g_settings->getBool("doubletap_jump");
 	m_cache_toggle_sneak_key             = g_settings->getBool("toggle_sneak_key");
 	m_cache_toggle_aux1_key              = g_settings->getBool("toggle_aux1_key");
-	m_cache_enable_joysticks             = g_settings->getBool("enable_joysticks");
 	m_cache_enable_fog                   = g_settings->getBool("enable_fog");
 	m_cache_mouse_sensitivity            = g_settings->getFloat("mouse_sensitivity", 0.001f, 10.0f);
-	m_cache_keyboard_camera_speed        = g_settings->getFloat("keyboard_camera_speed", 0.001f, 720.0f);
-	m_cache_joystick_frustum_sensitivity = std::max(g_settings->getFloat("joystick_frustum_sensitivity"), 0.001f);
 	m_repeat_place_time                  = g_settings->getFloat("repeat_place_time", 0.16f, 2.0f);
 	m_repeat_dig_time                    = g_settings->getFloat("repeat_dig_time", 0.0f, 2.0f);
 

builtin/common/settings/dlg_settings.lua
@@ -639,11 +639,16 @@ local function get_formspec(dialogdata)
 					default = fgettext_ne("Disabled")
 				end
 			elseif comp.setting.type == "key" then
-				default = (default ~= "")
-					and core.get_key_description(default)
-
+				local keys = default:split("|")
+				for k, v in pairs(keys) do
+					keys[k] = core.get_key_description(v)
+				end
+				if #keys > 0 then
+					default = table.concat(keys, " | ")
+				else
 					-- TRANSLATORS: Indicates that the action does not have a corresponding keybinding.
-					or fgettext_ne("Not bound")
+					default = fgettext_ne("Not bound")
+				end
 			else
 				local sinfo = get_setting_info(comp.setting.name)
 				if sinfo and sinfo.option_labels and sinfo.option_labels[default] then

irr/include/IEventReceiver.h
@@ -47,15 +47,11 @@ enum EEVENT_TYPE
 	//! A device motion event.
 	EET_DEVICE_MOTION_EVENT,
 
-	//! A joystick (joypad, gamepad) input event.
-	/** Joystick events are created by polling all connected joysticks once per
-	device run() and then passing the events to IrrlichtDevice::postEventFromUser.
-	They take the same path as mouse events.
-	Windows, SDL: Implemented.
-	Linux: Implemented, with POV hat issues.
-	MacOS / Other: Not yet implemented.
-	*/
-	EET_JOYSTICK_INPUT_EVENT,
+	//! A gamepad button event.
+	EET_GAMEPAD_BUTTON_EVENT,
+
+	//! A gamepad axis event.
+	EET_GAMEPAD_AXIS_EVENT,
 
 	//! A log event
 	/** Log events are only passed to the user receiver if there is one. If they are absorbed by the
@@ -460,63 +456,30 @@ struct SEvent
 		f64 Z;
 	};
 
-	//! A joystick event.
-	/** Unlike other events, joystick events represent the result of polling
-	 * each connected joystick once per run() of the device. Joystick events will
-	 * not be generated by default.  If joystick support is available for the
-	 * active device, and @ref IrrlichtDevice::activateJoysticks() has been
-	 * called, an event of this type will be generated once per joystick per
-	 * @ref IrrlichtDevice::run() regardless of whether the state of the joystick
-	 * has actually changed. */
-	struct SJoystickEvent
+	//! A gamepad event.
+	struct SGamepadButtonEvent
 	{
-		enum
-		{
-			NUMBER_OF_BUTTONS = 32,
+		//! The ID of the gamepad.
+		u32 ID;
 
-			AXIS_X = 0, // e.g. analog stick 1 left to right
-			AXIS_Y,     // e.g. analog stick 1 top to bottom
-			AXIS_Z,     // e.g. throttle, or analog 2 stick 2 left to right
-			AXIS_R,     // e.g. rudder, or analog 2 stick 2 top to bottom
-			AXIS_U,
-			AXIS_V,
-			NUMBER_OF_AXES = 18 // (please tell Irrlicht maintainers if you absolutely need more axes)
-		};
+		//! The button that is pressed or released.
+		GamepadButton Button;
 
-		/** A bitmap of button states.  You can use IsButtonPressed() to
-		 ( check the state of each button from 0 to (NUMBER_OF_BUTTONS - 1) */
-		u32 ButtonStates;
+		//! Whether the button is pressed.
+		bool PressedDown;
+	};
 
-		/** For AXIS_X, AXIS_Y, AXIS_Z, AXIS_R, AXIS_U and AXIS_V
-		 * Values are in the range -32768 to 32767, with 0 representing
-		 * the center position.  You will receive the raw value from the
-		 * joystick, and so will usually want to implement a dead zone around
-		 * the center of the range. Axes not supported by this joystick will
-		 * always have a value of 0. On Linux, POV hats are represented as axes,
-		 * usually the last two active axis.
-		 */
-		s16 Axis[NUMBER_OF_AXES];
+	//! A gamepad axis event.
+	struct SGamepadAxisEvent
+	{
+		//! The ID of the gamepad.
+		u32 ID;
 
-		/** The POV represents the angle of the POV hat in degrees * 100,
-		 * from 0 to 35,900.  A value of 65535 indicates that the POV hat
-		 * is centered (or not present).
-		 * This value is only supported on Windows.  On Linux, the POV hat
-		 * will be sent as 2 axes instead. */
-		u16 POV;
+		//! The axis that was changed.
+		GamepadAxis Axis;
 
-		//! The ID of the joystick which generated this event.
-		/** This is an internal Irrlicht index; it does not map directly
-		 * to any particular hardware joystick. */
-		u8 Joystick;
-
-		//! A helper function to check if a button is pressed.
-		bool IsButtonPressed(u32 button) const
-		{
-			if (button >= (u32)NUMBER_OF_BUTTONS)
-				return false;
-
-			return (ButtonStates & (1 << button)) ? true : false;
-		}
+		//! The value of the axis.
+		s16 Value;
 	};
 
 	//! Any kind of log event.
@@ -559,7 +522,8 @@ struct SEvent
 		struct SAccelerometerEvent AccelerometerEvent;
 		struct SGyroscopeEvent GyroscopeEvent;
 		struct SDeviceMotionEvent DeviceMotionEvent;
-		struct SJoystickEvent JoystickEvent;
+		struct SGamepadButtonEvent GamepadButtonEvent;
+		struct SGamepadAxisEvent GamepadAxisEvent;
 		struct SLogEvent LogEvent;
 		struct SUserEvent UserEvent;
 		struct SApplicationEvent ApplicationEvent;
@@ -568,7 +532,7 @@ struct SEvent
 	SEvent() {
 		EventType = static_cast<EEVENT_TYPE>(0);
 		// zero the biggest union member we have, which clears all others too
-		memset(&JoystickEvent, 0, sizeof(JoystickEvent));
+		memset(&AccelerometerEvent, 0, sizeof(AccelerometerEvent));
 	}
 };
 
@@ -592,39 +556,3 @@ public:
 	*/
 	virtual bool OnEvent(const SEvent &event) = 0;
 };
-
-//! Information on a joystick, returned from @ref IrrlichtDevice::activateJoysticks()
-struct SJoystickInfo
-{
-	//! The ID of the joystick
-	/** This is an internal Irrlicht index; it does not map directly
-	 * to any particular hardware joystick. It corresponds to the
-	 * SJoystickEvent Joystick ID. */
-	u8 Joystick;
-
-	//! The name that the joystick uses to identify itself.
-	core::stringc Name;
-
-	//! The number of buttons that the joystick has.
-	u32 Buttons;
-
-	//! The number of axes that the joystick has, i.e. X, Y, Z, R, U, V.
-	/** Note: with a Linux device, the POV hat (if any) will use two axes. These
-	 *  will be included in this count. */
-	u32 Axes;
-
-	//! An indication of whether the joystick has a POV hat.
-	/** A Windows device will identify the presence or absence of the POV hat.
-	 *  A Linux device cannot, and will always return POV_HAT_UNKNOWN. */
-	enum
-	{
-		//! A hat is definitely present.
-		POV_HAT_PRESENT,
-
-		//! A hat is definitely not present.
-		POV_HAT_ABSENT,
-
-		//! The presence or absence of a hat cannot be determined.
-		POV_HAT_UNKNOWN
-	} PovHat;
-}; // struct SJoystickInfo

irr/include/IrrlichtDevice.h
@@ -249,16 +249,6 @@ public:
 	//! Get the position of the frame on-screen
 	virtual core::position2di getWindowPosition() = 0;
 
-	//! Activate any joysticks, and generate events for them.
-	/** Irrlicht contains support for joysticks, but does not generate joystick events by default,
-	as this would consume joystick info that 3rd party libraries might rely on. Call this method to
-	activate joystick support in Irrlicht and to receive SJoystickEvent events.
-	\param joystickInfo On return, this will contain an array of each joystick that was found and activated.
-	\return true if joysticks are supported on this device, false if joysticks are not
-				 supported or support is compiled out.
-	*/
-	virtual bool activateJoysticks(core::array<SJoystickInfo> &joystickInfo) = 0;
-
 	//! Activate accelerometer.
 	virtual bool activateAccelerometer(float updateInterval = 0.016666f) = 0;
 
@@ -368,5 +358,15 @@ public:
 	virtual Keycode getKeyFromScancode(const u32 scancode) const {
 		(void)scancode;
 		return Keycode(KEY_UNKNOWN, (wchar_t)0xFFFF);
+	}
+
+	//! Get the label of a button on a gamepad.
+	/** This is a wrapper for SDL_GamepadButtonLabel.
+	\param button The gamepad button.
+	\return The corresponding label, or 0 if no sensible value can be determined.
+	*/
+	virtual GamepadButtonLabel getGamepadButtonLabel(const GamepadButton button) const {
+		// Dummy implementation
+		return GamepadButtonLabel::UNKNOWN;
 	}
 };

irr/include/Keycodes.h
@@ -200,3 +200,61 @@ public:
 		return code > 0 && code < KEY_KEY_CODES_COUNT;
 	}
 };
+
+/// A GamepadAxis represents a physical axis on the gamepad.
+/// The values are taken from SDL_GamepadAxis.
+enum class GamepadAxis {
+	LEFTX,
+	LEFTY,
+	RIGHTX,
+	RIGHTY,
+	LEFT_TRIGGER,
+	RIGHT_TRIGGER,
+	COUNT
+};
+
+/// A GamepadButton represents a physical button on the gamepad.
+/// The values are taken from SDL_GamepadButton.
+enum class GamepadButton {
+	SOUTH,
+	EAST,
+	WEST,
+	NORTH,
+	BACK,
+	GUIDE,
+	START,
+	LEFT_STICK,
+	RIGHT_STICK,
+	LEFT_SHOULDER,
+	RIGHT_SHOULDER,
+	DPAD_UP,
+	DPAD_DOWN,
+	DPAD_LEFT,
+	DPAD_RIGHT,
+	MISC1,
+	RIGHT_PADDLE1,
+	LEFT_PADDLE1,
+	RIGHT_PADDLE2,
+	LEFT_PADDLE2,
+	TOUCHPAD,
+	MISC2,
+	MISC3,
+	MISC4,
+	MISC5,
+	MISC6,
+	COUNT
+};
+
+/// A GamepadButtonLabel represents the label on the gamepad button.
+/// The values are taken from SDL_GamepadButtonLabel.
+enum class GamepadButtonLabel {
+	UNKNOWN,
+	A,
+	B,
+	X,
+	Y,
+	CROSS,
+	CIRCLE,
+	SQUARE,
+	TRIANGLE
+};

irr/src/CGUIEnvironment.cpp
@@ -547,6 +547,8 @@ bool CGUIEnvironment::postEventFromUser(const SEvent &event)
 		}
 	} break;
 	case EET_STRING_INPUT_EVENT:
+	case EET_GAMEPAD_AXIS_EVENT:
+	case EET_GAMEPAD_BUTTON_EVENT:
 		if (Focus && Focus->OnEvent(event))
 			return true;
 		break;

irr/src/CIrrDeviceSDL.cpp
@@ -38,12 +38,13 @@
 
 #ifndef _IRR_USE_SDL3_
 	// SDL2 backwards compatibility for things that were renamed in SDL3.
+	#define SDL_INIT_GAMEPAD SDL_INIT_GAMECONTROLLER
+
 	#define SDL_KMOD_SHIFT KMOD_SHIFT
 	#define SDL_KMOD_CTRL  KMOD_CTRL
 	#define SDL_KMOD_NUM   KMOD_NUM
 
 	#define SDL_TextInputActive(unused) SDL_IsTextInputActive()
-	#define SDL_CloseJoystick SDL_JoystickClose
 	#define SDL_GL_DestroyContext SDL_GL_DeleteContext
 
 	#define SDL_WINDOW_HIGH_PIXEL_DENSITY SDL_WINDOW_ALLOW_HIGHDPI
@@ -70,17 +71,18 @@
 	#define SDL_EVENT_RENDER_TARGETS_RESET SDL_RENDER_TARGETS_RESET
 	#define SDL_EVENT_RENDER_DEVICE_LOST SDL_RENDER_DEVICE_RESET
 
-	#define SDL_UpdateJoysticks SDL_JoystickUpdate
-	#define SDL_GetNumJoystickButtons SDL_JoystickNumButtons
+	#define SDL_EVENT_GAMEPAD_ADDED SDL_CONTROLLERDEVICEADDED
+	#define SDL_EVENT_GAMEPAD_REMOVED SDL_CONTROLLERDEVICEREMOVED
+	#define SDL_EVENT_GAMEPAD_BUTTON_DOWN SDL_CONTROLLERBUTTONDOWN
+	#define SDL_EVENT_GAMEPAD_BUTTON_UP SDL_CONTROLLERBUTTONUP
+	#define SDL_EVENT_GAMEPAD_AXIS_MOTION SDL_CONTROLLERAXISMOTION
 
-	#define SDL_GetNumJoystickAxes SDL_JoystickNumAxes
-	#define SDL_GetJoystickButton SDL_JoystickGetButton
-	#define SDL_GetJoystickAxis SDL_JoystickGetAxis
-	#define SDL_GetNumJoystickHats SDL_JoystickNumHats
-	#define SDL_GetJoystickHat SDL_JoystickGetHat
-
-	#define SDL_OpenJoystick SDL_JoystickOpen
-	#define SDL_GetJoystickName SDL_JoystickName
+	#define SDL_OpenGamepad SDL_GameControllerOpen
+	#define SDL_CloseGamepad SDL_GameControllerClose
+	#define SDL_GetGamepadName SDL_GameControllerName
+	#define SDL_GetGamepadMapping SDL_GameControllerMapping
+	#define SDL_GetGamepadType SDL_GameControllerGetType
+	#define SDL_GetGamepadStringForType(type) nullptr
 
 	#define SDL_GetWindowSizeInPixels SDL_GL_GetDrawableSize
 	#define SDL_DestroySurface SDL_FreeSurface
@@ -336,6 +338,28 @@ Keycode CIrrDeviceSDL::getKeyFromScancode(const u32 scancode) const
 	return Keycode(irrcode, keychar);
 }
 
+GamepadButtonLabel CIrrDeviceSDL::getGamepadButtonLabel(const GamepadButton button) const
+{
+#if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_) && defined(_IRR_USE_SDL3_)
+	if (auto gamepad = getRecentGamepad(); gamepad != nullptr)
+		return static_cast<GamepadButtonLabel>(SDL_GetGamepadButtonLabel(gamepad, static_cast<SDL_GamepadButton>(button)));
+#endif
+	return GamepadButtonLabel::UNKNOWN;
+}
+
+#if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_) && defined(_IRR_USE_SDL3_)
+SDL_Gamepad *CIrrDeviceSDL::getRecentGamepad() const
+{
+	auto p = gamepads.find(recentGamepadID);
+	if (p != gamepads.end())
+		return p->second;
+	p = gamepads.begin();
+	if (p != gamepads.end())
+		return p->second;
+	return nullptr;
+}
+#endif
+
 void CIrrDeviceSDL::resetReceiveTextInputEvents()
 {
 	gui::IGUIElement *elem = GUIEnvironment->getFocus();
@@ -437,6 +461,7 @@ CIrrDeviceSDL::CIrrDeviceSDL(const SIrrlichtCreationParameters &param) :
 			flags |= SDL_INIT_VIDEO;
 #if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_)
 		flags |= SDL_INIT_JOYSTICK;
+		flags |= SDL_INIT_GAMEPAD;
 #endif
 
 #ifdef _IRR_USE_SDL3_
@@ -489,9 +514,8 @@ CIrrDeviceSDL::CIrrDeviceSDL(const SIrrlichtCreationParameters &param) :
 CIrrDeviceSDL::~CIrrDeviceSDL()
 {
 #if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_)
-	const u32 numJoysticks = Joysticks.size();
-	for (u32 i = 0; i < numJoysticks; ++i)
-		SDL_CloseJoystick(Joysticks[i]);
+	for (const auto &p: gamepads)
+		SDL_CloseGamepad(p.second);
 #endif
 	if (Window && Context) {
 		SDL_GL_MakeCurrent(Window, NULL);
@@ -1118,137 +1142,84 @@ bool CIrrDeviceSDL::run()
 			os::Printer::log("Received SDL_RENDER_DEVICE_RESET. Rendering is probably broken.", ELL_ERROR);
 			break;
 
+#if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_)
+		case SDL_EVENT_GAMEPAD_ADDED: {
+#ifdef _IRR_USE_SDL3_
+			auto id = SDL_event.gdevice.which;
+#else
+			auto id = SDL_event.cdevice.which;
+#endif
+			if (auto gamepad = SDL_OpenGamepad(id); gamepad != nullptr) {
+				gamepads.emplace(id, gamepad);
+				recentGamepadID = id;
+				os::Printer::log("Gamepad connected", SDL_GetGamepadName(gamepad), ELL_INFORMATION);
+				os::Printer::log("Gamepad type", SDL_GetGamepadStringForType(SDL_GetGamepadType(gamepad)), ELL_INFORMATION);
+				if (auto mapping = SDL_GetGamepadMapping(gamepad); mapping != nullptr) {
+					os::Printer::log("Gamepad mapping", mapping, ELL_INFORMATION);
+					SDL_free(mapping);
+				}
+			} else {
+				os::Printer::log("Unable to open gamepad", SDL_GetError(), ELL_ERROR);
+			}
+			break;
+		}
+
+		case SDL_EVENT_GAMEPAD_REMOVED: {
+#ifdef _IRR_USE_SDL3_
+			auto id = SDL_event.gdevice.which;
+#else
+			auto id = SDL_event.cdevice.which;
+#endif
+			if (auto p = gamepads.find(id); p != gamepads.end()) {
+				os::Printer::log("Gamepad disconnected", SDL_GetGamepadName(p->second), ELL_INFORMATION);
+				SDL_CloseGamepad(p->second);
+				gamepads.erase(id);
+			}
+			break;
+		}
+
+		case SDL_EVENT_GAMEPAD_BUTTON_DOWN:
+		case SDL_EVENT_GAMEPAD_BUTTON_UP: {
+			irrevent.EventType = EET_GAMEPAD_BUTTON_EVENT;
+#ifdef _IRR_USE_SDL3_
+			auto id = SDL_event.gbutton.which;
+			irrevent.GamepadButtonEvent.Button = static_cast<GamepadButton>(SDL_event.gbutton.button);
+#else
+			auto id = SDL_event.cbutton.which;
+			irrevent.GamepadButtonEvent.Button = static_cast<GamepadButton>(SDL_event.cbutton.button);
+#endif
+			irrevent.GamepadButtonEvent.ID = id;
+			irrevent.GamepadButtonEvent.PressedDown = (SDL_event.type == SDL_EVENT_GAMEPAD_BUTTON_DOWN);
+			recentGamepadID = id;
+			postEventFromUser(irrevent);
+			break;
+		}
+
+		case SDL_EVENT_GAMEPAD_AXIS_MOTION: {
+			irrevent.EventType = EET_GAMEPAD_AXIS_EVENT;
+#if _IRR_USE_SDL3_
+			auto id = SDL_event.gaxis.which;
+			irrevent.GamepadAxisEvent.Axis = static_cast<GamepadAxis>(SDL_event.gaxis.axis);
+			irrevent.GamepadAxisEvent.Value = SDL_event.gaxis.value;
+#else
+			auto id = SDL_event.caxis.which;
+			irrevent.GamepadAxisEvent.Axis = static_cast<GamepadAxis>(SDL_event.caxis.axis);
+			irrevent.GamepadAxisEvent.Value = SDL_event.caxis.value;
+#endif
+			irrevent.GamepadAxisEvent.ID = id;
+			recentGamepadID = id;
+			postEventFromUser(irrevent);
+			break;
+		}
+#endif // _IRR_COMPILE_WITH_JOYSTICK_EVENTS_
+
 		default:
 			break;
 		} // end switch
 		resetReceiveTextInputEvents();
 	} // end while
 
-#if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_)
-	// TODO: Check if the multiple open/close calls are too expensive, then
-	// open/close in the constructor/destructor instead
-
-	// update joystick states manually
-	SDL_UpdateJoysticks();
-	// we'll always send joystick input events...
-	SEvent joyevent;
-	joyevent.EventType = EET_JOYSTICK_INPUT_EVENT;
-	for (u32 i = 0; i < Joysticks.size(); ++i) {
-		SDL_Joystick *joystick = Joysticks[i];
-		if (joystick) {
-			int j;
-			// query all buttons
-			const int numButtons = core::min_(SDL_GetNumJoystickButtons(joystick), 32);
-			joyevent.JoystickEvent.ButtonStates = 0;
-			for (j = 0; j < numButtons; ++j)
-				joyevent.JoystickEvent.ButtonStates |= (SDL_GetJoystickButton(joystick, j) << j);
-
-			// query all axes, already in correct range
-			const int numAxes = core::min_(SDL_GetNumJoystickAxes(joystick),
-					(int)SEvent::SJoystickEvent::NUMBER_OF_AXES);
-			joyevent.JoystickEvent.Axis[SEvent::SJoystickEvent::AXIS_X] = 0;
-			joyevent.JoystickEvent.Axis[SEvent::SJoystickEvent::AXIS_Y] = 0;
-			joyevent.JoystickEvent.Axis[SEvent::SJoystickEvent::AXIS_Z] = 0;
-			joyevent.JoystickEvent.Axis[SEvent::SJoystickEvent::AXIS_R] = 0;
-			joyevent.JoystickEvent.Axis[SEvent::SJoystickEvent::AXIS_U] = 0;
-			joyevent.JoystickEvent.Axis[SEvent::SJoystickEvent::AXIS_V] = 0;
-			for (j = 0; j < numAxes; ++j)
-				joyevent.JoystickEvent.Axis[j] = SDL_GetJoystickAxis(joystick, j);
-
-			// we can only query one hat, SDL only supports 8 directions
-			if (SDL_GetNumJoystickHats(joystick) > 0) {
-				switch (SDL_GetJoystickHat(joystick, 0)) {
-				case SDL_HAT_UP:
-					joyevent.JoystickEvent.POV = 0;
-					break;
-				case SDL_HAT_RIGHTUP:
-					joyevent.JoystickEvent.POV = 4500;
-					break;
-				case SDL_HAT_RIGHT:
-					joyevent.JoystickEvent.POV = 9000;
-					break;
-				case SDL_HAT_RIGHTDOWN:
-					joyevent.JoystickEvent.POV = 13500;
-					break;
-				case SDL_HAT_DOWN:
-					joyevent.JoystickEvent.POV = 18000;
-					break;
-				case SDL_HAT_LEFTDOWN:
-					joyevent.JoystickEvent.POV = 22500;
-					break;
-				case SDL_HAT_LEFT:
-					joyevent.JoystickEvent.POV = 27000;
-					break;
-				case SDL_HAT_LEFTUP:
-					joyevent.JoystickEvent.POV = 31500;
-					break;
-				case SDL_HAT_CENTERED:
-				default:
-					joyevent.JoystickEvent.POV = 65535;
-					break;
-				}
-			} else {
-				joyevent.JoystickEvent.POV = 65535;
-			}
-
-			// we map the number directly
-			joyevent.JoystickEvent.Joystick = static_cast<u8>(i);
-			// now post the event
-			postEventFromUser(joyevent);
-			// and close the joystick
-		}
-	}
-#endif
 	return !Close;
-}
-
-//! Activate any joysticks, and generate events for them.
-bool CIrrDeviceSDL::activateJoysticks(core::array<SJoystickInfo> &joystickInfo)
-{
-#if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_)
-	joystickInfo.clear();
-
-	int numJoysticks = 0;
-#ifdef _IRR_USE_SDL3_
-	(void)SDL_GetJoysticks(&numJoysticks);
-#else
-	numJoysticks = SDL_NumJoysticks();
-#endif
-	// we can name up to 256 different joysticks
-	numJoysticks = core::min_(numJoysticks, 256);
-
-	Joysticks.reallocate(numJoysticks);
-	joystickInfo.reallocate(numJoysticks);
-
-	int joystick = 0;
-	for (; joystick < numJoysticks; ++joystick) {
-		Joysticks.push_back(SDL_OpenJoystick(joystick));
-		SJoystickInfo info;
-
-		info.Joystick = joystick;
-		info.Axes = SDL_GetNumJoystickAxes(Joysticks[joystick]);
-		info.Buttons = SDL_GetNumJoystickButtons(Joysticks[joystick]);
-		info.Name = SDL_GetJoystickName(Joysticks[joystick]);
-		info.PovHat = (SDL_GetNumJoystickHats(Joysticks[joystick]) > 0)
-							  ? SJoystickInfo::POV_HAT_PRESENT
-							  : SJoystickInfo::POV_HAT_ABSENT;
-
-		joystickInfo.push_back(info);
-	}
-
-	for (joystick = 0; joystick < (int)joystickInfo.size(); ++joystick) {
-		char logString[256];
-		snprintf_irr(logString, sizeof(logString), "Found joystick %d, %d axes, %d buttons '%s'",
-				joystick, joystickInfo[joystick].Axes,
-				joystickInfo[joystick].Buttons, joystickInfo[joystick].Name.c_str());
-		os::Printer::log(logString, ELL_INFORMATION);
-	}
-
-	return true;
-
-#endif // _IRR_COMPILE_WITH_JOYSTICK_EVENTS_
-
-	return false;
 }
 
 void CIrrDeviceSDL::updateSizeAndScale()

irr/src/CIrrDeviceSDL.h
@@ -24,8 +24,14 @@
 #include <SDL.h>
 #endif
 
+#include <map>
 #include <memory>
 #include <unordered_map>
+
+#ifndef _IRR_USE_SDL3_
+	// Backward compatibility for SDL2
+	#define SDL_Gamepad SDL_GameController
+#endif
 
 class CIrrDeviceSDL : public CIrrDeviceStub
 {
@@ -94,9 +100,6 @@ public:
 
 	//! Get the position of this window on screen
 	core::position2di getWindowPosition() override;
-
-	//! Activate any joysticks, and generate events for them.
-	bool activateJoysticks(core::array<SJoystickInfo> &joystickInfo) override;
 
 	//! Get the device type
 	E_DEVICE_TYPE getType() const override
@@ -296,6 +299,11 @@ public:
 		gui::ECURSOR_ICON ActiveIcon = gui::ECURSOR_ICON::ECI_NORMAL;
 	};
 
+	u32 getScancodeFromKey(const Keycode &key) const override;
+	Keycode getKeyFromScancode(const u32 scancode) const override;
+
+	GamepadButtonLabel getGamepadButtonLabel(const GamepadButton button) const override;
+
 private:
 #ifdef _IRR_EMSCRIPTEN_PLATFORM_
 	static EM_BOOL MouseUpDownCallback(int eventType, const EmscriptenMouseEvent *event, void *userData);
@@ -308,9 +316,6 @@ private:
 
 	// Return the Char that should be sent to Irrlicht for the given key (either the one passed in or 0).
 	static wchar_t findCharToPassToIrrlicht(uint32_t sdlKey, EKEY_CODE irrlichtKey, u16 keymod);
-
-	u32 getScancodeFromKey(const Keycode &key) const override;
-	Keycode getKeyFromScancode(const u32 scancode) const override;
 
 	// Check if a text box is in focus. Enable or disable SDL_TEXTINPUT events only if in focus.
 	void resetReceiveTextInputEvents();
@@ -327,8 +332,14 @@ private:
 	SDL_GLContext Context;
 	SDL_Window *Window;
 #if defined(_IRR_COMPILE_WITH_JOYSTICK_EVENTS_)
-	core::array<SDL_Joystick *> Joysticks;
-#endif
+	std::map<SDL_JoystickID, SDL_Gamepad*> gamepads;
+#ifdef _IRR_USE_SDL3
+	SDL_JoystickID recentGamepadID = -1;
+#else
+	SDL_JoystickID recentGamepadID = 0;
+#endif // _IRR_USE_SDL3_
+	SDL_Gamepad *getRecentGamepad() const;
+#endif // _IRR_COMPILE_WITH_JOYSTICK_EVENTS_
 
 	s32 MouseX, MouseY;
 	// these two only continue to exist for some Emscripten stuff idk about

commit d33b3454860ab717cc6cfc2b51e9118b5c82ddbb

Fix autoforward regression with get_player_control().up (#17504)
Fixes #17502

Regression from #17219

src/client/game.cpp
@@ -2055,8 +2055,15 @@ void Game::updatePlayerControl(const CameraOrientation &cam)
 
 	//TimeTaker tt("update player control", NULL, PRECISION_NANO);
 
+	float forward_value = getAxisValue(KeyType::FORWARD);
+	if (player->getPlayerSettings().continuous_forward &&
+			client->activeObjectsReceived() && !player->isDead()) {
+		// autoforward if set: move at maximum speed
+		forward_value = 1.0f;
+	}
+
 	PlayerControl control(
-		getAxisValue(KeyType::FORWARD),
+		forward_value,
 		getAxisValue(KeyType::BACKWARD),
 		getAxisValue(KeyType::LEFT),
 		getAxisValue(KeyType::RIGHT),
@@ -2070,15 +2077,6 @@ void Game::updatePlayerControl(const CameraOrientation &cam)
 		cam.camera_yaw
 	);
 	control.setMovementFromKeys();
-
-	// autoforward if set: move at maximum speed
-	if (player->getPlayerSettings().continuous_forward &&
-			client->activeObjectsReceived() && !player->isDead()) {
-		control.movement_speed = 1.0f;
-		// sideways movement only
-		float dx = std::sin(control.movement_direction);
-		control.movement_direction = std::atan2(dx, 1.0f);
-	}
 
 	/* For touch, simulate holding down AUX1 (fast move) if the user has
 	 * the fast_move setting toggled on. If there is an aux1 key defined for