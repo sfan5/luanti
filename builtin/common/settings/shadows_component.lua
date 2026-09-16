6392cae9e3030b662dc81f159ead4326b92321f6
sfan5 <sfan5@live.de>
2026-09-16 16:43:19 +0200

    Settings: ensure relevant setting notes are visible even with search

--- builtin/common/settings/shadows_component.lua
+++ builtin/common/settings/shadows_component.lua
@@ -67,11 +67,7 @@ end
 
 
 return {
-	query_text = "Shadows",
-	requires = {
-		shadows_support = true,
-	},
-	context = "client",
+	-- no 'requires' or 'context' needed here because the component is not standalone
 	get_formspec = function(self, avail_w)
 		local labels = table.copy(shadow_levels_labels)
 		local idx = detect_mapping_idx()
