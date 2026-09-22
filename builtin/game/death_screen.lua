925fa33f2fd61cd9d764f5c4528e5bb58e806c65
CrazyladMT <247920740+CrazyladMT@users.noreply.github.com>
2026-09-22 15:15:25 -0400

    Fix death screen text centering for older clients (#17580)

--- builtin/game/death_screen.lua
+++ builtin/game/death_screen.lua
@@ -2,15 +2,29 @@ local F = core.formspec_escape
 local S = core.get_translator("__builtin")
 
 function core.show_death_screen(player, _reason)
+	local name = player:get_player_name()
+	local info = core.get_player_information(name)
+	local ver  = info.formspec_version
+
 	local fs = {
 		"formspec_version[2]",
 		"size[14.25,7.275,true]",
-		"bgcolor[#320000b4;true]",
-		"style_type[label;halign=center;valign=center]",
-		"label[5.375,1.85;3.5,0.8;", F(S("You died")), "]",
-		"button_exit[5.375,3.725;3.5,0.8;btn_respawn;", F(S("Respawn")), "]",
+		"bgcolor[#320000b4;true]"
 	}
-	core.show_formspec(player:get_player_name(), "__builtin:death", table.concat(fs, ""))
+
+	if ver >= 11 then
+		-- use a proper centered label if the client supports it
+		table.insert(fs, "style_type[label;halign=center;valign=center]")
+		table.insert(fs, "label[5.375,1.85;3.5,0.8;" .. F(S("You died")) .. "]")
+	else
+		-- fallback: use a borderless button to center the text
+		table.insert(fs, "style_type[button;border=false]")
+		table.insert(fs, "button[5.375,1.85;3.5,0.8;;" .. F(S("You died")) .. "]")
+		table.insert(fs, "style_type[button;border=true]")
+	end
+
+	table.insert(fs, "button_exit[5.375,3.725;3.5,0.8;btn_respawn;" .. F(S("Respawn")) .. "]")
+	core.show_formspec(name, "__builtin:death", table.concat(fs, ""))
 end
 
 core.register_on_dieplayer(function(player, reason)
